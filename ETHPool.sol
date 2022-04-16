3c301b0f1dc50b1b4503f6f59c27a8224a78cb65
matsblock <96005274+matsblock@users.noreply.github.com>
2022-04-16 12:16:48 -0300

    Update ETHPool.sol

--- ETHPool.sol
+++ ETHPool.sol
@@ -6,79 +6,129 @@ contract ETHPool  {
 
     // Function to receive Ether. msg.data must be empty
     receive() external payable {}
-
-    // Fallback function is called when msg.data is not empty
-    fallback() external payable {}
-
+    uint public _rewards;
+    uint public userRewards;
+    uint public _totalUserDepositsInThisReward;
+//    uint public depositsLength;
     //------
     address public owner;
 
-    mapping (address => uint256) public BalanceOf;
-    uint256 public totalBalance;
-    uint256 public dividendPerPool;
-    mapping (address => uint256) public dividendBalanceOf;
+    mapping (address => uint) public BalanceOf;
+    uint public totalUsersBalance;
 
-    mapping (address => uint256) public RewardsBalanceOf;
     uint256 public totalRewardsBalance;
 
     struct depositRegistryStruct{
-        uint depositTime;
-        uint depositId;
-        uint amount;
-    }
-    mapping (address => depositRegistryStruct[]) public depositRegistry;
-    mapping (address => depositRegistryStruct[]) public depositRewardsRegistry;
+         uint depositTime;
+         uint depositId;
+         uint amount;
+         uint totalBalanceAtThisTime;
+//         uint balanceAtThisTime;
+
+
+     }
+    mapping (address => depositRegistryStruct[]) public depositsUserRegistry;
+    depositRegistryStruct[] public rewardsDepositsRegistry;
+    mapping (address => uint) public lastClaim;
+
+
+    // mapping (address => uint256) public lastClaim;
 
     constructor() {
         // Set the transaction sender as the owner of the contract.
         owner = msg.sender;
+ //       lastClaim[msg.sender] = 0;
     }
 
-    function deposit() public payable {
+    function userDeposit() public payable {
+        totalUsersBalance += msg.value; 
+        BalanceOf[msg.sender] += msg.value;
+        depositRegistryStruct memory userDeposit;
+
+         userDeposit.amount = msg.value;
+         userDeposit.depositTime = block.timestamp;
+         userDeposit.depositId = depositsUserRegistry[msg.sender].length;
+         userDeposit.totalBalanceAtThisTime = totalUsersBalance;
+         depositsUserRegistry[msg.sender].push(userDeposit);
+
         (bool sent, bytes memory data) = address(this).call{value: msg.value}("");
         require(sent, "Failed to send Ether");
-
-        totalBalance += msg.value; 
-        BalanceOf[msg.sender] += msg.value;
-        uint a;
-        
-       depositRegistryStruct memory _depositRegistryStruct;
-
-        _depositRegistryStruct.amount = msg.value;
-        _depositRegistryStruct.depositTime = block.timestamp;
-        _depositRegistryStruct.depositId = depositRegistry[msg.sender].length;
-        depositRegistry[msg.sender].push(_depositRegistryStruct);
     }
+
+    // function userDeposit(address _address ,uint _depositId) public returns (depositRegistryStruct memory) {
+    //     return depositsUserRegistry[_address][_depositId];
+    // }
 
     function rewardsDeposit() public payable onlyOwner  {
         totalRewardsBalance += msg.value; 
-        depositRegistryStruct memory _depositRegistryStruct;
+        depositRegistryStruct memory rewardDeposit;
 
-        _depositRegistryStruct.amount = msg.value;
-        _depositRegistryStruct.depositTime = block.timestamp;
-        _depositRegistryStruct.depositId = depositRewardsRegistry[msg.sender].length;
-        depositRewardsRegistry[msg.sender].push(_depositRegistryStruct);
+         rewardDeposit.amount = msg.value;
+         rewardDeposit.depositTime = block.timestamp;
+         rewardDeposit.depositId = rewardsDepositsRegistry.length;
+         rewardDeposit.totalBalanceAtThisTime = totalUsersBalance;
+         rewardsDepositsRegistry.push(rewardDeposit);
 
         (bool sent, bytes memory data) = address(this).call{value: msg.value}("");
         require(sent, "Failed to send Ether");
     }
 
-    //Withdraw Ether from the contract
-    function withdrawDeposit(uint256 amount) public {
+    // function rewardDeposit(uint _depositId) public returns (depositRegistryStruct memory) {
+    //     return rewardsDepositsRegistry[_depositId];
+    // }
 
-    totalBalance -= amount; 
-    BalanceOf[msg.sender] -= amount;
-    
-    (bool succeed, bytes memory data) = msg.sender.call{value: amount}("");
-    require(succeed, "Failed to withdraw Ether");
+    //Withdraw Ether from the contract
+    function withdrawDeposit(uint amount) public {
+        totalUsersBalance -= amount; 
+        BalanceOf[msg.sender] -= amount;
+        
+         (bool succeed, bytes memory data) = msg.sender.call{value: amount}("");
+         require(succeed, "Failed to withdraw Ether");
 }
+
+
+ function calculateRewards() public returns (uint)  {
+    uint rewards; 
+    uint totalUserDeposits; 
+    uint proportionRewards;
+
+        for (uint i = rewardsDepositsRegistry.length; i > 0 ; i--) {  
+            if (lastClaim[msg.sender] < rewardsDepositsRegistry[i - 1].depositTime) {
+                totalUserDeposits = calculateTotalUserDepositsInReward(i - 1);
+                proportionRewards += totalUserDeposits * 100 /  rewardsDepositsRegistry[i-1].totalBalanceAtThisTime * rewardsDepositsRegistry[i-1].amount;
+            }
+            else {
+                _rewards = 0;
+                break;
+            }      
+//               _rewards = i - 1;
+    }
+
+//            rewards = proportionRewards;
+            _rewards = proportionRewards;
+            lastClaim[msg.sender]=block.timestamp;
+            return _rewards;
+        }
+
+function calculateTotalUserDepositsInReward(uint i) public returns (uint){
+    uint totalUserDepositsInThisReward = 0; 
+            for (uint j = 0 ; j < depositsUserRegistry[msg.sender].length; j++){
+                    if (rewardsDepositsRegistry[i].depositTime > depositsUserRegistry[msg.sender][j].depositTime){
+                        totalUserDepositsInThisReward +=  depositsUserRegistry[msg.sender][j].amount;
+                     } 
+                     else {
+                         break;
+                        }
+                }
+        _totalUserDepositsInThisReward = totalUserDepositsInThisReward;
+        return totalUserDepositsInThisReward;
+    }
+
 
 function claimRewards() public {
     uint rewards;
-    dividendBalanceOf[msg.sender] = BalanceOf[msg.sender]  * 100 / totalBalance;
-    RewardsBalanceOf[msg.sender] = dividendBalanceOf[msg.sender] * totalRewardsBalance / 100;
-    rewards = RewardsBalanceOf[msg.sender];
-
+    rewards = calculateRewards() / 100;
+    lastClaim[msg.sender] = block.timestamp;
 
     (bool succeed, bytes memory data) = msg.sender.call{value: rewards}("");
     require(succeed, "Failed to withdraw Ether");
@@ -86,8 +136,8 @@ function claimRewards() public {
  
     //Send ether from the account connected with this function
     function sendViaCall(address payable _to) public payable {
-        // Call returns a boolean value indicating success or failure.
-        // This is the current recommended method to use.
+    // Call returns a boolean value indicating success or failure.
+    // This is the current recommended method to use.
         (bool sent, bytes memory data) = _to.call{value: msg.value}("");
         require(sent, "Failed to send Ether");
     }
@@ -95,6 +145,12 @@ function claimRewards() public {
     function getContractBalance() public view returns (uint) {
         return address(this).balance;
     }
+
+    function getLegthDepositsArray() public view returns (uint){
+        return  depositsUserRegistry[msg.sender].length;
+    }
+
+    
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not owner");
@@ -104,4 +160,10 @@ function claimRewards() public {
         _;
     }
 
+    function testSomething() public returns (uint) {
+        uint i = 1;
+        return  (i - 1);
+    }
+ 
+}
  
